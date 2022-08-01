cc153c3750183b65da6e3d77403f3a7945e37215
lukasmasuch <lukas.masuch@gmail.com>
2022-08-01 12:26:06 +0200

    more changes

--- lib/streamlit/app_session.py
+++ lib/streamlit/app_session.py
@@ -722,7 +722,8 @@ def _populate_user_info_msg(msg: UserInfo) -> None:
 
 
 def _populate_app_pages(
-    msg: Union[NewSession, PagesChanged], main_script_path: str
+    msg: Union[NewSession, PagesChanged], 
+main_script_path: str
 ) -> None:
     for page_script_hash, page_info in source_util.get_pages(main_script_path).items():
         page_proto = msg.app_pages.add()
@@ -730,3 +731,4 @@ def _populate_app_pages(
         page_proto.page_script_hash = page_script_hash
         page_proto.page_name = page_info["page_name"]
         page_proto.icon = page_info["icon"]
+
